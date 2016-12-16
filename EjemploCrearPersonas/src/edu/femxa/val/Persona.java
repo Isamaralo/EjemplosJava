607c1bba6166639f3e8d135d79072109795014db
isamaralo <isa-vdm@hotmail.com>
2016-12-16 16:49:39 +0100

    commit

---
+++ EjemploCrearPersonas/src/edu/femxa/val/Persona.java
@@ -0,0 +1,13 @@
+package edu.femxa.val;
+
+public class Persona {
+	String nombre;
+	int edad;
+	
+	public Persona (String nombre_recibido, int edad_recibida)
+	{
+		nombre = nombre_recibido;
+		edad = edad_recibida;
+	}
+
+}
