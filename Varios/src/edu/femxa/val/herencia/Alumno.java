e0253fa8b5f1eabb119539f418ea2f738965b247
isamaralo <isa-vdm@hotmail.com>
2016-12-20 21:27:12 +0100

    commit

--- Varios/src/edu/femxa/val/herencia/Alumno.java
+++ Varios/src/edu/femxa/val/herencia/Alumno.java
@@ -21,6 +21,22 @@ public class Alumno extends Persona{
 		// TODO Auto-generated method stub
 		//return super.toString(); //Llamo al método del padre
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		boolean b_dev = false;
+		Alumno alumno_object = null;
+
+			alumno_object = (Alumno) obj;
+			if((this.getNombre().equals(alumno_object.getNombre())) 
+					&& (this.getEdad() == alumno_object.getEdad()) 
+					&& (this.nota == alumno_object.nota))
+						b_dev = true;
+		
+		return b_dev;
+		// TODO Auto-generated method stub
+		//return super.equals(obj);
+	}
 
 	public int getNota() {
 		return nota;
--- Varios/src/edu/femxa/val/herencia/MainHerencia.java
+++ Varios/src/edu/femxa/val/herencia/MainHerencia.java
@@ -5,6 +5,7 @@ public class MainHerencia {
 	public static void main(String[] args) {
 		
 		Alumno alumno = new Alumno();
+		Alumno alumno2 = new Alumno();
 		
 		alumno.setNombre("Nira");
 		alumno.setEdad(26);
@@ -13,6 +14,18 @@ public class MainHerencia {
 		alumno.mostrarPersona();
 		System.out.println("HASHCODE: " +alumno.hashCode());
 		System.out.println("TOSTRING: " +alumno.toString());
+		
+		alumno2.setNombre("Nira");
+		alumno2.setNota(5);
+		alumno2.setEdad(27);
+		
+		//alumno2 = alumno;
+		
+		if (alumno.equals(alumno2))
+			System.out.println("Alumno y Alumno2 son iguales");
+		
+		else
+			System.out.println("Alumno y Alumno2 NO son iguales");
 		
 		if (alumno instanceof Persona)
 			System.out.println("alumno es objeto de la clase Persona");
