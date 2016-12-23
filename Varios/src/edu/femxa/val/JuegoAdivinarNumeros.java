40fae99b91cc2168fd567bb0a1b376b1971a806f
isamaralo <isa-vdm@hotmail.com>
2016-12-23 15:43:35 +0100

    commit

--- Varios/src/edu/femxa/val/JuegoAdivinarNumeros.java
+++ Varios/src/edu/femxa/val/JuegoAdivinarNumeros.java
@@ -37,26 +37,45 @@ public class JuegoAdivinarNumeros {
 	}
 	
 	/**
-	 * Compara el dos números, el que se ha introducido con el aleatorio, y muestra 
-	 * cual es mayor y el número de intentos que le quedan al usuario.
-	 * @param num_introducido Número que se lee desde teclado.
-	 * @param num_aleatorio Número que se genera aleatoriamente y es el que hay que adivinar.
-	 * @param intentos Número de veces que puedes intentar adivinar el número.
+	 * Compara dos números y devuelve una cadena con el resultado.
+	 * @param num_introducido Uno de los números a comparar.
+	 * @param num_aleatorio Otro de los números a comparar.
+	 * @return Mayor, menor o igual.
 	 */
-	public static void compararNumeros(int num_introducido, int num_aleatorio, int intentos)
+	public static String compararNumeros(int num_introducido, int num_aleatorio)
 	{
 		String resultado = null;
+		
 		if (num_introducido < num_aleatorio)
-		{
+			resultado = "mayor"; 
+			else if (num_introducido > num_aleatorio)
+					resultado = "menor";
+				else if (num_introducido == num_aleatorio)
+					resultado = "igual";
+		
+		return resultado;
+	}
+	
+	/**
+	 * Muestra un mensaje con el resultado y el número de intentos que le quedan al usuario.
+	 * @param resultado Mensaje a imprimir.
+	 * @param intentos Número de intentos que le quedan al usuario.
+	 */
+	public static void mostrarResultados(String resultado, int intentos)
+	{
+		switch (resultado){
+		case "mayor": 
 			System.out.println("El número aleatorio es mayor. Te quedan " + (5-intentos) + " oportunidades");
-		}
-		else if (num_introducido > num_aleatorio)
-		{
+			break;
+		case "menor":
 			System.out.println("El número aleatorio es menor. Te quedan " + (5-intentos) + " oportunidades");
-		}
-		else if (num_introducido == num_aleatorio)
-		{
+			break;
+		case "igual":
 			System.out.println("Acertaste!!");
+			break;
+		default:
+				break;
+		
 		}
 	}
 	
@@ -65,13 +84,15 @@ public class JuegoAdivinarNumeros {
 		int num_aleatorio = generarAleatorio(); 
 		int intentos = 0;
 		int num_introducido = 0;
+		String resultado = null;
 		System.out.println("Se ha generado un número aleatorio entre 1 y 100. Adivina el número (tienes 5 oportunidades): ");
 		
 		do
 		{
 			num_introducido = pedirNumero();
 			intentos++;
-			compararNumeros(num_introducido, num_aleatorio, intentos);
+			resultado = compararNumeros(num_introducido, num_aleatorio);
+			mostrarResultados(resultado, intentos);
 		}
 		while ((intentos < 5) && (num_introducido != num_aleatorio));
 		
