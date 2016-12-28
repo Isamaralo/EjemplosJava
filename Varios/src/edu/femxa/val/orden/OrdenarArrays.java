1ca010aa199f9c92b69effa62d634c8b0964a880
isamaralo <isa-vdm@hotmail.com>
2016-12-28 18:53:43 +0100

    commit

--- Varios/src/edu/femxa/val/orden/OrdenarArrays.java
+++ Varios/src/edu/femxa/val/orden/OrdenarArrays.java
@@ -1,24 +1,11 @@
 package edu.femxa.val.orden;
 
-import java.util.Scanner;
-
 public class OrdenarArrays {
     
     public static int generarAleatorio ()
     {
         int aleatorio = (int) ((100 * Math.random())+1);
         return aleatorio;
-    }
-    
-    public static int pedirNumeroUsuario ()
-    {
-        int numero_usuario = 0;
-        Scanner scanner = null;
-            
-            scanner = new Scanner(System.in);
-            numero_usuario = scanner.nextInt();
-            
-        return numero_usuario;
     }
     
     public static void ordenBurbuja (int[] a_ent_a)
@@ -55,7 +42,6 @@ public class OrdenarArrays {
         }
     }
     
-    
     public static void main(String[] args) {
         
         int a_ent_a[] = new int[10];
@@ -77,29 +63,6 @@ public class OrdenarArrays {
         for (int i = 0; i < a_ent_a.length; i++) {
             System.out.println("Num en pos " + (i+1) + " " + a_ent_a[i]);
         }
-        
-        
-        //Pedir 10 números al usuario y que los inserte de menor a mayor
-        int longitud = a_ent_a.length;
-        for (int i = 0; i < longitud; i++) {
-            int num_usuario = 0;
-                
-            System.out.println("Introduzca un número: ");
-            num_usuario = pedirNumeroUsuario();
-            a_ent_a[i] = num_usuario;
-            
-            int j = i-1;
-            while (j>0 || a_ent_a[j]>a_ent_a[i])
-            {
-                a_ent_a[j+1] = a_ent_a[j];
-                j--;
-            }
-                
-        }
-        
-
-        
-        
     }
 
 }
