69b3f8627a2e80ff94aefbd5389af0ff4e24a574
isamaralo <isa-vdm@hotmail.com>
2017-01-23 19:41:34 +0100

    commit

--- Varios/src/edu/femxa/val/jdbc/Consultas.java
+++ Varios/src/edu/femxa/val/jdbc/Consultas.java
@@ -4,5 +4,22 @@ public class Consultas {
 
 	public static final String CONSULTA_EMPLEADOS_SALARIO_DESCENDENTE = "SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.SALARY, E.DEPARTMENT_ID, D.DEPARTMENT_NAME FROM DEPARTMENTS D, EMPLOYEES E WHERE D.DEPARTMENT_ID=E.DEPARTMENT_ID ORDER BY E.SALARY DESC";
 	public static final String CONSULTA_EMPLEADOS_ADMINISTRACION = "SELECT E.EMPLOYEE_ID, E.FIRST_NAME, E.SALARY, E.DEPARTMENT_ID, D.DEPARTMENT_NAME FROM DEPARTMENTS D, EMPLOYEES E WHERE D.DEPARTMENT_ID=E.DEPARTMENT_ID AND D.DEPARTMENT_NAME = 'Administration'";
-
+	public static final String CONSULTA_EMPLEADOS_SALARIO_ASC = "SELECT * from EMPLOYEES ORDER BY SALARY DESC";
+	public static final String 
+	CONSULTA_EMPLEADOS_SALARIO_ADMON_E_IT 
+	= "SELECT first_name, salary FROM "
+			+ "EMPLOYEES where department_id IN "
+			+ "(SELECT department_id FROM "
+			+ "DEPARTMENTS WHERE department_name = "
+			+ "'Administration' or "
+			+ "department_name = 'IT')";
+	
+	public static final String 
+	ACTUALIZAR_EMPLEADOS_SALARIO_ADMON_E_IT 
+	= "UPDATE EMPLOYEES SET salary = (salary * 1.2) "
+			+ "where department_id IN "
+			+ "(SELECT department_id FROM "
+			+ "DEPARTMENTS WHERE department_name = "
+			+ "'Administration' or "
+			+ "department_name = 'IT')";
 }
